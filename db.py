ec1fdacbfa2b3dd04d0ab1a7c6476ff1cd03e2cd
Menilek <menilek25@hotmail.co.uk>
2018-12-20 15:37:03 +0000

    styled view.html using materialize

--- db.py
+++ db.py
@@ -1,9 +1,7 @@
 from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import exc
 
-#Parses data and changes the format of the datetime
 def formatReadingDataForDisplay(data):
     formattedTime = data[0].strftime('%H:%M %d/%m/%Y')
     litres = data[1]
@@ -11,7 +9,6 @@ def formatReadingDataForDisplay(data):
     items = [formattedTime, litres, percent]
     return(items)
 
-#Retrieves the latest row entered into the database
 def getLastReadingFromDB():
     queryData = db.engine.execute("SELECT date_trunc('second', created_on), litres, percentage FROM Reading ORDER BY id DESC LIMIT 1").first()
     formattedData = formatReadingDataForDisplay(queryData)
@@ -22,22 +19,20 @@ def handleError(data):
     db.session.add(data)
     db.session.flush()
 
-#Takes a list of data and inserts each element of the list into a column in the table
 def insertNewReadingToDB(waterData):
     reading = Reading(waterData[0], waterData[1])
-    db.session.add(reading)
     try:
+        db.session.add(reading)
         db.session.commit()
-    except exc.IntegrityError:
+        db.session.close()
+    except:
         handleError(reading)
 
 app = Flask(__name__)
-#Heroku db details susceptible to change
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://water:waterPi@localhost:1234'
+app.config.from_object('config')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-#alter table Reading ADD id integer PRIMARY KEY, ADD datetime timestamp [without time zone] NOT NULL, ADD litres numeric NOT NULL, percentage integer NOT NULL
 class Reading(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     litres = db.Column(db.Numeric, nullable = False)
